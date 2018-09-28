82b2394f350ce4b554dd11aa51757d037969524a
jermken <jermken@163.com>
2018-09-28 19:36:14 +0800

    '消费单页面'

--- src/pages/payment/index.vue
+++ src/pages/payment/index.vue
@@ -2,14 +2,41 @@
     <div class="payment-page">
         <div class="serving-user">服务中客户</div>
         <div class="serving-bill">
-            <div>客户姓名</div>
-            <div>服务员工</div>
+            <el-form :model="billInfo" status-icon :rules="formRules" ref="staffInfoForm" :size="globalSize">
+                <el-form-item label="客户类别">
+                    <el-radio-group v-model="billInfo.userType">
+                        <el-radio label="1">游客</el-radio>
+                        <el-radio label="2">客户</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="游客姓名" v-if="billInfo.userType == 1">
+                    <el-input placeholder="请输入游客姓名"></el-input>
+                </el-form-item>
+                <el-form-item label="客户姓名" v-else>
+                    <el-input placeholder="请输入客户姓名"></el-input>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    name: 'Payment'
+    name: 'Payment',
+    data() {
+        return {
+            billInfo: {
+                userType: '1'
+            },
+            formRules: {
+
+            }
+        }
+    },
+    computed: {
+        ...mapGetters(['globalSize'])
+    }
 }
 </script>
 <style lang="scss">
